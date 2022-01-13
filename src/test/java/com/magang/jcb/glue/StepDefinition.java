076c7e96cc212509efdfe3eb99bc9164bef97397
rezapahlevihaikal <eskrimayambawang@gmail.com>
2022-01-13 11:52:38 +0700

    Update Login page

--- src/test/java/com/magang/jcb/glue/StepDefinition.java
+++ src/test/java/com/magang/jcb/glue/StepDefinition.java
@@ -1,26 +1,34 @@
 package com.magang.jcb.glue;
 
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.magang.jcb.configs.AutomationFrameworkConfigJava;
 import com.magang.jcb.drivers.DriverSingleton;
+import com.magang.jcb.pages.LoginPages;
 import com.magang.jcb.utils.ConfigurationProperties;
 import com.magang.jcb.utils.Constans;
 import com.magang.jcb.utils.TestCases;
 import com.magang.jcb.utils.Utility;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
 @ContextConfiguration(classes = AutomationFrameworkConfigJava.class)
 public class StepDefinition {
 	private WebDriver driver;
+	private LoginPages login;
 	TestCases[] testCases;
 	ExtentTest extentTest;
 	static ExtentReports report = new ExtentReports("src/main/resources/TestReport.html");
@@ -30,18 +38,52 @@ public class StepDefinition {
 
 	@Before
 	public void initializeObjects() {
-		DriverSingleton.getInstance(Constans.CHROME);
+		DriverSingleton.getInstance(configProperties.getBrowser());
 		driver = DriverSingleton.getDriver();
+		login = new LoginPages();
 		testCases = TestCases.values();
 		this.extentTest = report.startTest(testCases[Utility.testCount].getTestName());
 		Utility.testCount++;
 	}
-
-	@After
-	public void closeObjects() {
-		report.endTest(this.extentTest);
-		report.flush();
-
+	
+	//============================ Login Activity =======================================
+	
+	@Given("^Menampilkan form login")
+	public void Menampilkan_form_login() {
+		driver = DriverSingleton.getDriver();
+		driver.get(Constans.URL);
+		extentTest.log(LogStatus.PASS,"Navigating to " + Constans.URL);
 	}
+	
+	@When("^Mengisi username")
+	public void Mengisi_username() {
+		login.inputUsername(configProperties.getUsername());
+		extentTest.log(LogStatus.PASS,"Mengisi username");
+	}
+	
+	@When("^Mengisi Password")
+	public void Mengisi_Password() {
+		login.inputPassword(configProperties.getPassword());
+		extentTest.log(LogStatus.PASS,"Mengisi Password");
+	}
+	
+	@When("^Login ke sistem JCB")
+	public void Login_ke_sistem_JCB() {
+		login.clickButtonLogin();
+		extentTest.log(LogStatus.PASS,"Login ke sistem JCB");
+	}
+	
+	@Then("^Muncul pesan selamat datang superadmin2")
+	public void Muncul_pesan_selamat_datang_superadmin2() {
+		assertEquals(configProperties.getMessageLogin(), login.getMessageText());
+		extentTest.log(LogStatus.PASS,"Muncul pesan selamat datang superadmin2");
+	}
+
+//	@After
+//	public void closeObjects() {
+//		report.endTest(this.extentTest);
+//		report.flush();
+//
+//	}
 
 }
